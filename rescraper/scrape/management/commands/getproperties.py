15b57bba17400d3a0b7a49d92e3fce6ba783e214
chris.holliday@gmail.com <chris.holliday@gmail.com>
2016-06-02 13:53:46 -0500

    fixed addresses and price per foot

--- rescraper/scrape/management/commands/getproperties.py
+++ rescraper/scrape/management/commands/getproperties.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import locale
 
 from decimal import Decimal
 from re import sub
@@ -36,13 +37,44 @@ class Command(BaseCommand):
                 desc += item.get_text(strip=False) + ", "
 
             # Convert string to decimal
+            # set local to convert string to int
+            locale.setlocale(locale.LC_ALL, 'en-US')
             price_decimal = Decimal(sub(r'[^\d.]', '', prop.find("li", class_="srp-item-price").getText(strip=True)))
+            square_feet = locale.atoi(prop.find("li", attrs={"data-label": "property-meta-sqft"}).span.getText(strip=True))
+            persquarefoot = price_decimal/square_feet
 
             Property.objects.get_or_create(
                 detail_url=prefix + prop.a["href"],
-                address=prop.a.getText(strip=True),
-                external_identifier=prop.a.getText(strip=True),
+                address=self.get_formatted_address(prop),
+                external_identifier= self.get_formatted_address(prop),
                 price=price_decimal,
                 features=desc,
                 is_multifamily=True,
+                google_map_url="https://www.google.com/maps/place/" + self.get_formatted_address(prop),
+                price_per_foot = persquarefoot,
             )
+
+            prop.find()
+
+    def get_formatted_address(self, prop):
+        try:
+            street = prop.find("span", class_="listing-street-address").getText()
+        except AttributeError:
+            street = "no street address"
+
+        try:
+            city = prop.find("span", class_="listing-city").getText()
+        except AttributeError:
+            city = "no city"
+
+        try:
+            state = prop.find("span", class_="listing-region").getText()
+        except AttributeError:
+            state = "no state"
+
+        try:
+            zip = prop.find("span", class_="listing-postal").getText()
+        except AttributeError:
+            zip = "no zip"
+
+        return street + ", " + city + ", " + state + " " + zip
